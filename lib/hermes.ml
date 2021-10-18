3e0267ef0642ec18d76d22c5e44bd177a843f37f
José Eduardo <josee.edumont@gmail.com>
2021-10-17 21:07:53 -0300

    feat: add subscriptions endpoint

--- lib/hermes.ml
+++ lib/hermes.ml
@@ -1,6 +1,17 @@
-let health_check_handler _ = Dream.respond ~code:200 ""
+let health_check_handler _ = Dream.empty `OK
+
+let subscribe_handler request =
+  match%lwt Dream.form ~csrf:false request with
+  | `Ok [ ("email", email); ("name", name) ] ->
+      Dream.respond ~status:`OK
+        (Format.sprintf "Email is: %s, name is: %s" email name)
+  | _ -> Dream.respond ~code:400 "Something went wrong"
 
 let server () =
-  Dream.run
-  @@ Dream.router [ Dream.get "/health_check" health_check_handler ]
+  Dream.run @@ Dream.logger
+  @@ Dream.router
+       [
+         Dream.get "/health_check" health_check_handler;
+         Dream.post "/subscriptions" subscribe_handler;
+       ]
   @@ Dream.not_found
