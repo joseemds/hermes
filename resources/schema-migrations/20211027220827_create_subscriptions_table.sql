32816ca4eafd158488bc8f49157589ed9b7b9132
José Eduardo <josee.edumont@gmail.com>
2021-10-28 23:13:05 -0300

    migrations: change timestamp to timestampz

--- resources/schema-migrations/20211027220827_create_subscriptions_table.sql
+++ resources/schema-migrations/20211027220827_create_subscriptions_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS subscriptions(
 	id uuid PRIMARY KEY NOT NULL,
 	email TEXT NOT NULL UNIQUE,
 	name TEXT NOT NULL,
-	subscribed_at timestamp NOT NULL
+	subscribed_at timestamptz NOT NULL
 )
 
